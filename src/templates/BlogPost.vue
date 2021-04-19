7db621755c17cf4c084695cee42efc8768eef7bc
Kim Jiho <go2zo.ooo@gmail.com>
2021-04-19 17:12:07 +0900

    Modified styles

--- src/templates/BlogPost.vue
+++ src/templates/BlogPost.vue
@@ -1,10 +1,10 @@
 <template>
   <Layout>
     <Section container="md" dots>
-      <div class="post-header">
+      <div class="text-center post-header mb-x2">
         <h1 v-html="$page.post.title" />
       </div>
-      <div class="post-content">
+      <div class="post-content post mb-x2">
         <div v-html="$page.post.content" />
       </div>
     </Section>
