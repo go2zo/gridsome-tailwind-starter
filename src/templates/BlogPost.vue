dbf3f71944d426db0f6d033044fe4b48fd20e64e
Kim Jiho <go2zo.ooo@gmail.com>
2021-04-21 16:11:28 +0900

    Import PostMeta in BlogPost

--- src/templates/BlogPost.vue
+++ src/templates/BlogPost.vue
@@ -3,6 +3,7 @@
     <Section container="md" dots>
       <div class="text-center post-header mb-x2">
         <h1 v-html="$page.post.title" />
+        <PostMeta :post="$page.post" />
       </div>
       <div class="post-content post mb-x2">
         <div v-html="$page.post.content" />
@@ -18,7 +19,21 @@ query($id: ID!) {
     date(format: "D. MMMM YYYY")
     timeToRead
     content
+    author {
+      id
+      title
+    }
     excerpt
   }
 }
 </page-query>
+
+<script>
+import PostMeta from '@/components/PostMeta'
+
+export default {
+  components: {
+    PostMeta,
+  }
+}
+</script>
