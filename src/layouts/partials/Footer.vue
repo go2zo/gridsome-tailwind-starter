6b772374c41d2d50a9fc3058024aa7c41294f3f0
Kim Jiho <go2zo.ooo@gmail.com>
2021-05-22 18:03:54 +0900

    [#1] Footer에 author 미표시

--- src/layouts/partials/Footer.vue
+++ src/layouts/partials/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-  <footer class="bg border-primary z-10 p-5 -mt-px text-xs text-center border">
+  <footer class="z-10 p-5 -mt-px text-xs text-center border bg border-primary">
     <span>© {{ new Date().getFullYear() }} </span>
-    <g-link to="/about">{{ $static.metadata.author.name }}.</g-link>
+    <g-link to="/about">{{ $static.metadata.author.title }}.</g-link>
     <span>
       Powered by
       <a href="//gridsome.org" rel="noopener noreferrer" target="_blank">
