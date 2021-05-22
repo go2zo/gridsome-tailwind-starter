de16f7d1be656d422a233d026d2eb3480f70db87
Kim Jiho <go2zo.ooo@gmail.com>
2021-05-22 18:06:45 +0900

    [#2] git commit massage hook

--- scripts/add_issue_number_into_commit.sh
+++ scripts/add_issue_number_into_commit.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 COMMIT_MSG_FILE=$1
-
 branch_name=`git rev-parse --abbrev-ref HEAD`
 cut_number=`echo ${branch_name} | cut -d '-' -f1`
 first_line=`head -n1 ${COMMIT_MSG_FILE}`
@@ -26,6 +25,6 @@ fi
 # if commit_msg_file's first line is empty,
 # add branch name in commit_msg
 if [ -z "${first_line}" ]; then
-  recent_commits >> $1
-  sed -i ".bak" "1s/^/[#${cut_number}] ${COMMIT_MSG}/" $1
+  recent_commits >> ${COMMIT_MSG_FILE}
+  sed -i ".bak" "1s/^/[#${cut_number}] ${COMMIT_MSG}/" ${COMMIT_MSG_FILE}
 fi
